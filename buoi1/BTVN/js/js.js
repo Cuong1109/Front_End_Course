e3220a69dbebc3426c3a0ee27747cf6a326798ce
Cuong1109 <teo9999kb@gmail.com>
2021-08-03 15:33:51 +0700

    Update F4.1

---
+++ buoi1/BTVN/js/js.js
@@ -0,0 +1,22 @@
+//Bai1
+
+// let yourName=prompt("Hi there, Enter your name here? ");
+// alert("Hi "+ yourName);
+
+//Bai2 
+// let firstName=prompt("Enter your first name: ");
+// let lastName=prompt("Enter your last name: ");
+// alert("Hi "+lastName+" "+firstName);
+
+// //Bai3
+// let lengthSquare=prompt("Enter side length of square: ");
+// alert(`The square area is ${lengthSquare*lengthSquare}`);
+
+//Bai4
+// let radiusCircle=prompt("Enter side radius of circle: ");
+// alert(`The circle area is ${radiusCircle*radiusCircle*3.14}`);
+
+//Bai5
+let temperatureCencius=prompt("Enter the temperature: ");
+let temparatureFarenheit=temperatureCencius*1.8+32;
+alert(`${temperatureCencius}(C)=${temparatureFarenheit}(F)`);
---
+++ buoi1/Tren_lop/js/js.js
@@ -0,0 +1,38 @@
+//var let const
+// var Name ="nhan";
+// Name=" cuong dep trai"
+// console.log(Name);
+// let age=1232142
+// console.log(age);
+
+// const a=1;
+// console.log(a);
+
+
+// let a=2;
+// let b='2';
+// let c=a+b;
+// let a= prompt("Nhap ten o day");
+// console.log(a);
+
+// let a=prompt("Nhap ten");
+// let b=prompt("Nhap ho");
+// let c=a+b;
+// alert(c);
+
+let a = Number(prompt("Nhap so thu 1: "));
+let b = Number(prompt('nhập số thứ 2:'));
+console.log("Chieu dai la "+a);
+console.log("Chieu rong la "+b)
+let c=a*b;
+let d=(a+b)*2;
+console.log("Dien tich của hinh chu nhat la "+c);
+console.log("Chu vi của hinh chu nhat la "+ d);
+console.log(`Chu vi la ${(a+b)*2}`);
+
+// let a=1;
+// let b=2;
+// console.log(`a la so: ${a+b}`);
+
+
+

6529f44d07bee21435937056f8cdc146446d9de0
Cuong1109 <teo9999kb@gmail.com>
2021-08-05 19:24:39 +0700

    Update buoi 2 tren lop

---
+++ buoi2/tren_lop/app.js
@@ -0,0 +1,13 @@
+// const boolean=true;
+// if (boolean==true)
+//     alert("Hello");
+// else
+//     alert("Khong phai true");
+
+let a='2';
+let b=2;
+if (a===b) {
+    alert("Cuong dz");
+} else {
+    alert("Cuong van dep zai")
+}
